e833f385d9113ae724452ad63ea1af277b78b9e1
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-14 16:29:02 +0300

    Ex13

---
+++ Ex13-HW/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Прога, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+*/
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+
+int ThirdDigitLeft(int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+
+if (number < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+    Console.WriteLine($"Третья цифра {ThirdDigitLeft(number)}");
+
+
+
+/*идеальное решение
+*/
+
+// int Prompt(string message)
+// {
+//     Console.WriteLine(message);
+//     int result = int.Parse(Console.ReadLine()!);
+//     return result;
+// }
+
+// int GetThirdRank(int number)
+// {
+//     while (number > 999)
+//     {
+//         number = number / 10;
+//     }
+//     return number % 10;
+// }
+
+// bool ValidateNumber(int number)
+// {
+//     if (number < 100)
+//     {
+//         Console.WriteLine("Третьей цифры нет");
+//         return false;
+//     }
+//     return true;
+// }
+
+// int number = Prompt("Введите число: ");
+// if (ValidateNumber(number))
+// {
+//     Console.WriteLine(GetThirdRank(number));
+// }
