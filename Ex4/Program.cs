0ed0b0d36b18bef7a96ec9f4e11f9159458314bd
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-14 16:28:38 +0300

    Ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Прога, которая на вход число и проверит, кратно ли оно одновременно 7 и 23
+*/
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine()!);
+    return num;
+}
+
+string Multiplicity(int number)
+{
+    string res = "No";
+    if (number % 7 == 0 && number % 23 == 0)
+    res = "Yes";
+    return res;
+}
+
+int num = InputNum("Введите целое число: ");
+string result = Multiplicity(num);
+Console. WriteLine(result);
