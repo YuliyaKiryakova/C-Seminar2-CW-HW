bd2bd2ac5c19e35592cab222390f3a7d012dc1bd
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-14 16:28:26 +0300

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите прогу, которая выводит трехзначное число и удаляет вторую цифру этого числа
+*/
+
+int RandomNum()
+{
+    int num = new Random().Next(100, 1000);
+    Console.WriteLine($"Сгенерировалось число: {num}");
+    return num;
+}
+
+// 123 -> 13
+// 123 % 10 = 3 - последняя
+// 123 / 100 = 1 - первая
+// 1 * 10 + 3 = 13
+
+int DelSecondDigit(int num)
+{
+    int first = num / 100;
+    int second = num % 10;
+    int twoDigits = first * 10 + second;
+    return twoDigits;
+}
+
+int number = RandomNum();
+int result = DelSecondDigit(number);
+Console.WriteLine($"Получилось {result}");
