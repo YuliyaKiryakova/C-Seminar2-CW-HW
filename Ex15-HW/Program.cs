8485381f6aa48830ae1650917496b7e9746a031b
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-14 16:29:16 +0300

    Ex15

---
+++ Ex15-HW/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Прога, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+*/
+
+Console.WriteLine("Введите цифру: ");
+int number = int.Parse(Console.ReadLine()!);
+
+bool Workdays(int num)
+{
+    if (num >= 1 && num <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Такого дня недели нет");
+    return false;
+}
+
+if (Workdays(number))
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Работаем...");
+    }
+}
+
+/* идеальное решение
+*/
+
+// Console.WriteLine("Введите цифру: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// bool InvalidWeekDay(int num)
+// {
+//     if (num > 0 && num <= 7)
+//     {
+//         return true;
+//     }
+//     Console.WriteLine("Нет такого дня недели");
+//     return false;
+// }
+
+// bool Weekend(int weekend)
+// {
+//     if (weekend > 5)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
+
+// if (InvalidWeekDay(number))
+// {
+//     if (Weekend(number))
+//     {
+//         Console.WriteLine("Выходной!");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Работаем...");
+//     }
+// }
