20c2ea01f52fc48e3ceabd802889fbf4c2833823
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-14 16:27:55 +0300

    Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая выводит случайное
+число из отрезка [10, 99] и показывает наибольшую цифру числа
+*/
+
+int RandomNum()
+{
+    int num = new Random().Next(10, 100);
+    Console.WriteLine($"Сгенерировалось число: {num}");
+    return num;
+}
+
+int MaxDigit(int number)
+{
+    int first = number / 10;
+    int second = number % 10;
+    if (first > second)
+        return first;
+    else
+        return second;
+}
+
+int rand = RandomNum();
+int maxDigit = MaxDigit(rand);
+Console.WriteLine($"Максимальная цифра в числе {rand} равна {maxDigit}");
