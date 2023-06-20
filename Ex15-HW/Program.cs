1c445ac387905f8fd94cca44188cd09b5ea27950
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-20 12:55:20 +0300

    update 15

--- Ex15-HW/Program.cs
+++ Ex15-HW/Program.cs
@@ -1,33 +1,67 @@
 ﻿/* Прога, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
 */
+/* без бул - Дмитрий подсказал*/
 
-Console.WriteLine("Введите цифру: ");
-int number = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите цифру: ");
+// int number = int.Parse(Console.ReadLine()!);
 
-bool Workdays(int num)
+// string Weekend(int num)
+// {
+//     string answer = "";
+//     if (num < 1 || num > 7)
+//         answer = "Такого дня недели нет";
+//     else if (num < 6)
+//         answer = "Работаем...";
+//     else
+//         answer = "Выходной!";
+//     return answer;
+// }
+
+// string res = Weekend(number);
+// Console.Write(res);
+
+
+void Weekend(int num)
 {
-    if (num >= 1 && num <= 7)
-    {
-        return true;
-    }
-    Console.WriteLine("Такого дня недели нет");
-    return false;
-}
-
-if (Workdays(number))
-{
-    if (number == 6 || number == 7)
-    {
-        Console.WriteLine("Выходной!");
-    }
-    else
-    {
+    if (num < 1 || num > 7)
+        Console.WriteLine("Такого дня недели нет");
+    else if (num < 6)
         Console.WriteLine("Работаем...");
-    }
+    else
+        Console.WriteLine("Выходной!");
 }
 
-/* идеальное решение
-*/
+Console.Write("Введите цифру: ");
+int number = int.Parse(Console.ReadLine()!);
+Weekend(number);
+
+/* c bool - списала с идеального по-своему*/
+
+// int number = int.Parse(Console.ReadLine()!);
+
+// bool Workdays(int num)
+// {
+//     if (num >= 1 && num <= 7)
+//     {
+//         return true;
+//     }
+//     Console.WriteLine("Такого дня недели нет");
+//     return false;
+// }
+
+// if (Workdays(number))
+// {
+//     if (number == 6 || number == 7)
+//     {
+//         Console.WriteLine("Выходной!");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Работаем...");
+//     }
+// }
+
+/* идеальное решение*/
 
 // Console.WriteLine("Введите цифру: ");
 // int number = int.Parse(Console.ReadLine()!);
